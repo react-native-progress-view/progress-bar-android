1b4a7f2b2baedcf06111dcf06553d5f2f98751a1
Vivian Fang <vf608@nyu.edu>
2019-05-01 17:11:36 -0400

    Fixing package names in Android files

--- android/src/main/java/com/reactnativecommunity/androidprogressbar/ReactProgressBarViewManager.java
+++ android/src/main/java/com/reactnativecommunity/androidprogressbar/ReactProgressBarViewManager.java
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.facebook.react.views.progressbar;
+package com.reactnativecommunity.progressbar;
 
 import javax.annotation.Nullable;
 

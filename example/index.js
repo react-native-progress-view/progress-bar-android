6b9e38671c034dcdd4736035957974387741cb0b
Vivian Fang <vf608@nyu.edu>
2019-05-06 17:51:54 -0400

    Linting

--- example/index.js
+++ example/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import {AppRegistry} from 'react-native';
 
 import {name as appName}  from './app.json';
-import ProgressBarAndroidExample from "./ProgressBarAndroidExample.android";
+import ProgressBarAndroidExample from './ProgressBarAndroidExample.android';
 
 class ExampleApp extends React.Component<{}> {
   render() {
     return (
         <ProgressBarAndroidExample />
-    )
+    );
   }
 }
 AppRegistry.registerComponent(appName, () => ExampleApp);

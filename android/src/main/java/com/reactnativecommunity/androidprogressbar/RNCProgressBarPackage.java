5652de10f68b49c33b16d22f0d9a357bf00ef801
mauriciopf <mauricio.perezflores@gmail.com>
2020-04-26 15:09:09 -0400

    add list to createViewManagers

--- android/src/main/java/com/reactnativecommunity/androidprogressbar/RNCProgressBarPackage.java
+++ android/src/main/java/com/reactnativecommunity/androidprogressbar/RNCProgressBarPackage.java
@@ -14,7 +14,7 @@ import com.facebook.react.bridge.JavaScriptModule;
 public class RNCProgressBarPackage implements ReactPackage {
   @Override
   public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-    return Arrays.<NativeModule>asList(new RNCProgressBarModule(reactContext));
+    return Collections.emptyList();
   }
 
   // Deprecated from RN 0.47
@@ -22,8 +22,9 @@ public class RNCProgressBarPackage implements ReactPackage {
     return Collections.emptyList();
   }
 
-  @Override
+ @Override
+  @SuppressWarnings("rawtypes")
   public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-    return Collections.emptyList();
+      return Arrays.<ViewManager>asList(new ReactProgressBarViewManager());
   }
 }
